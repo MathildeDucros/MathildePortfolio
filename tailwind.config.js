8685f0206ada7e293501e1fefb6f8bb14969ac0b
Mathilde <ducros.mathilde@gmail.com>
2023-05-16 18:24:19 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#171313",
+        secondary: "#E5BDBB",
+        tertiary: "#b24f60",
+        "black-100": "#170A0C",
+        "black-200": "#2F1419",
+        "white-100": "#F3F3F3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
